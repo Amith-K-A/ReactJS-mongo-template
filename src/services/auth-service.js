e74aa6fb0e1e3fb8651cc6a6fa58a56bd58f249e
Amith <amith.k.a@hp.com>
2021-04-02 23:54:23 +0530

    proxy fix

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/";
-
 const register = (username, email, password) => {
-  return axios.post(API_URL + "signup", {
+  return axios.post("/signup", {
     username,
     email,
     password,
@@ -12,7 +10,7 @@ const register = (username, email, password) => {
 
 const login = (username, password) => {
   return axios
-    .post(API_URL + "signin", {
+    .post( "/signin", {
       username,
       password,
     })
